853cf25c4b6176acae367ac63a4d9a73d042d00b
yousef2213 <mandooyousef988@gmail.com>
2020-11-30 10:25:57 +0200

    change_

--- src/components/HomeComponents/Sec2.js
+++ src/components/HomeComponents/Sec2.js
@@ -6,33 +6,33 @@ function Sec2() {
   return (
     <div className="container mx-auto">
       <div className="row mx-0 my-3 b_g py-2">
-        <div className="col col-6 col-sm-6 col-md-3 mx-auto d-flex my-3">
+        <div className="col col-6 col-sm-6 col-md-3 mx-auto d-flex my-3 dhover">
           <div>
             <img src={overs} className="icon_Sec2" alt="overs" />
           </div>
-          <div className="text-right">
+          <div className="text-right align-self-center">
             <h5 className="mb-0 font-main font-weight-bold nameSec2">
               العروض والخصومات
             </h5>
             <h6 className="font-main font-weight-bold mb-0 PSec2">دفع امن</h6>
           </div>
         </div>
-        <div className="col col-6 col-md-3 mx-auto d-flex my-3">
+        <div className="col col-6 col-md-3 mx-auto d-flex my-3 dhover">
           <div>
             <img src={overs2} className="icon_Sec2" alt="overs" />
           </div>
-          <div className="text-right">
+          <div className="text-right align-self-center">
             <h5 className="mb-0 nameSec2"> طرق الدفع</h5>
             <h6 className="font-main font-weight-bold mb-0 PSec2">
               خصومات وعروض دائمة
             </h6>
           </div>
         </div>
-        <div className="col col-6 col-md-3 mx-auto d-flex  my-3">
+        <div className="col col-6 col-md-3 mx-auto d-flex my-3 dhover">
           <div>
             <img src={overs3} className="icon_Sec2" alt="overs" />
           </div>
-          <div className="text-right">
+          <div className="text-right align-self-center">
             <h5 className="mb-0 font-main font-weight-bold nameSec2">
               التوصيل
             </h5>
@@ -41,11 +41,11 @@ function Sec2() {
             </h6>
           </div>
         </div>
-        <div className="col col-6 col-md-3 mx-auto d-flex  my-3">
+        <div className="col col-6 col-md-3 mx-auto d-flex my-3 dhover">
           <div>
             <img src={overs4} className="icon_Sec2" alt="overs" />
           </div>
-          <div className="text-right">
+          <div className="text-right align-self-center">
             <h5 className="mb-0 font-main font-weight-bold nameSec2">
               مركز المساعدة
             </h5>
